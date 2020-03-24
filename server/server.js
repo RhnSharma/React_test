b46736956d2b75b35416dfc1a5ed1989636906d7
Rohan Sharma <rhnsharma5113@gmail.com>
2020-03-24 20:39:36 +0530

    Heroku update 2

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(require('./routes'));
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
     // Set static folder
-    app.use('/',express.static(path.join(__dirname,'../client/build')));
+    app.use(express.static('../client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
