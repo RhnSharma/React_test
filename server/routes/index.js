f20f2d63d23bf55e2dc6af414cd9edeb05757fb4
Rohan Sharma <rhnsharma5113@gmail.com>
2020-03-25 11:29:34 +0530

    Update

--- server/routes/index.js
+++ server/routes/index.js
@@ -59,7 +59,7 @@ routeid.save()
 });
 req.session.errors = null;
 req.session.success = null;
-res.render('./../../client/build/index.html');
+res.render('./../../client/build/index');
 console.log(`Execution Time(GET /) is ${Date.now() - start}ms and ID is ${id}`.green);
 morgan(':date :method :url :status :response-time ms');
 logger.info(req.method,`APP STARTED | request id: ${id} | Execution Time : ${Date.now() - start}ms`);
