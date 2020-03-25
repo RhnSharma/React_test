302f678d19c81088902ca8e652ce3d6b38200eaf
Rohan Sharma <rhnsharma5113@gmail.com>
2020-03-25 19:01:13 +0530

    Update heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -30,41 +30,8 @@ router.use(morgan(':date :method :url :status :response-time ms'));
 router.use(helmet());
 router.use(compression());
 
-router.get('/', async  (req,res) => {
-let start = Date.now();
-let id = req._id;
-let routeid = new RouteID();
-await RouteID.findOne({name:req.originalUrl}, (err,doc) => { 
-if (err) throw err;
-if(doc)
-{
-id = doc.id;
-console.log('ID ALREADY EXISTS!'.green,id.red);
-} 
-else
-{
-routeid.id = uuidv4();
-routeid.name = req.originalUrl;
-id = routeid.id;
-routeid.save()
-.then(data=>{
-    console.log('ID CREATED'.green,data.id.red);
-    logger.info('ID CREATED',data.id.green);
-})
-.catch(err=> {
-    console.log('Error during ID creation : ' + err)
-    logger.error('ERROR DURING ID creation',err);
-});
-}
-});
-req.session.errors = null;
-req.session.success = null;
 router.use(function(req, res) {
 	res.sendFile(path.join(__dirname, './../../client/build/index.html'));
-});
-console.log(`Execution Time(GET /) is ${Date.now() - start}ms and ID is ${id}`.green);
-morgan(':date :method :url :status :response-time ms');
-logger.info(req.method,`APP STARTED | request id: ${id} | Execution Time : ${Date.now() - start}ms`);
 });
 
 module.exports = router;
