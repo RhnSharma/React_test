e7b2c48bbe2d0fda49e094f63e8184f0d15ec906
Rohan Sharma <rhnsharma5113@gmail.com>
2020-04-14 18:58:50 +0530

    Added a new project

--- client/src/components/ProjectsPage.js
+++ client/src/components/ProjectsPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import chatapp from '../images/chatapp.jpg';
 import calculator from '../images/jscalc.png';
+import mailerapp from '../images/mailerapp.jpg';
 import { IoMdFolder } from 'react-icons/io';
 import { FaExternalLinkSquareAlt, FaGithub } from 'react-icons/fa';
 import './../App.css';
@@ -12,6 +13,21 @@ const ProjectsPage = (props) => {
       <Row>
         <Col md="10">
         <h1 className='display-4 mb-3 text-info title' style={{'fontSize':'2rem'}}> <IoMdFolder /> Projects</h1>
+        <div className="card text-white bg-dark mb-4">
+          <img className="card-img-top" src={mailerapp} alt="Mailer app"></img>
+          <div className="card-body">
+            <h5 className="card-title">Mailer App</h5>
+            <p className="card-text">A mailer made using Nodejs, cron and nodemailer.</p>
+            <div className='sub'> 
+            <div className='subleft'>
+            <p className='text-muted'>nodejs &nbsp; cron &nbsp; nodemailer</p>
+            </div>
+            <div className='subright'>
+            <a href='https://github.com/RhnSharma/mailer' target='_blank' rel='noopener noreferrer'><FaGithub className='link' color='white' title='Github repository link'/></a>
+            </div>
+            </div>
+          </div>
+        </div>
         <div className="card text-white bg-dark mb-4">
           <img className="card-img-top" src={chatapp} alt="Chat app"></img>
           <div className="card-body">
