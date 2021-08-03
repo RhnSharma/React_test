6704eeb2fbf1fbeaca6ad65c5ed217182d6ee169
Rohan Sharma <rhnsharma5113@gmail.com>
2021-08-03 18:49:53 +0530

    add no data text

--- client/src/components/Submissions.js
+++ client/src/components/Submissions.js
@@ -6,6 +6,7 @@ import axios from "axios";
 
 const Submissions = (props) => {
   const [submissions, setSubmissions] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     axios
       .get("/getAll", {
@@ -13,7 +14,10 @@ const Submissions = (props) => {
           type: "application/json",
         },
       })
-      .then((res) => setSubmissions(res.data))
+      .then((res) => {
+        setLoading(false);
+        setSubmissions(res.data);
+      })
       .catch((err) => console.log(err));
   }, []);
   return (
@@ -24,25 +28,36 @@ const Submissions = (props) => {
             {" "}
             <IoMdPerson /> Submissions :{" "}
           </h1>
-          {submissions.length ? (
-            submissions.map((submission) => {
-              return (
-                <div
-                  key={submission.id}
-                  className="card text-light bg-dark mb-2"
-                  style={{ width: "25rem", height: "auto" }}
-                >
-                  <div className="card-body">
-                    <h5 className="card-title">{submission.name}</h5>
-                    <p className="card-text text-muted">
-                      {submission.createdAt}
-                    </p>
-                    <p className="card-text">{submission.email}</p>
-                    <p className="card-text">{submission.message}</p>
+          {!loading ? (
+            submissions.length > 0 ? (
+              submissions.map((submission) => {
+                return (
+                  <div
+                    key={submission.id}
+                    className="card text-light bg-dark mb-2"
+                    style={{ width: "25rem", height: "auto" }}
+                  >
+                    <div className="card-body">
+                      <h5 className="card-title">{submission.name}</h5>
+                      <p className="card-text text-muted">
+                        {submission.createdAt}
+                      </p>
+                      <p className="card-text">{submission.email}</p>
+                      <p className="card-text">{submission.message}</p>
+                    </div>
                   </div>
-                </div>
-              );
-            })
+                );
+              })
+            ) : (
+              <div
+                style={{
+                  margin: "10%",
+                  color: "#cb251d",
+                }}
+              >
+                <h2>No submissions!</h2>
+              </div>
+            )
           ) : (
             <div className="clearfix p-5 m-5">
               <div className="spinner-border float-left" role="status">
